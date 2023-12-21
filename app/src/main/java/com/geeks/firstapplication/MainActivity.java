15afc5698f4a0f74078416089281c5eb937d86a7
Albiinaa <turarbekovaalbina382@gmail.com>
2023-12-21 15:41:53 +0600

    First Commit

---
+++ app/src/main/java/com/geeks/firstapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.geeks.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.firstapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.edit_text);
+                String text = editText.getText().toString();
+                TextView textView = findViewById(R.id.text_view);
+                textView.setText(text);
+            }
+        });
+    }
+}
